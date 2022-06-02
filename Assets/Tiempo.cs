06b18a79081c3fe1e943f18e684fb958b29985d0
Uriel Jolodosvky <46960341@est.ort.edu.ar>
2022-06-02 20:24:16 -0300

    Contador hecho

---
+++ Assets/Tiempo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tiempo : MonoBehaviour
+{
+    public Text txt_customTime;
+    float customTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        customTime = 120;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        customTime -= Time.deltaTime;
+        txt_customTime.text = Mathf.Floor(customTime).ToString(); 
+    }
+}
