53bc6352a89ab03f3d1bbc109ea758a36601730d
DylanTsai1 <101568962+DylanTsai1@users.noreply.github.com>
2022-06-07 22:45:40 -0300

    Panel que dice quien hizo gol

--- Assets/Colisiones.cs
+++ Assets/Colisiones.cs
@@ -11,53 +11,70 @@ public class Colisiones : MonoBehaviour
     public Text puntaje2;
     public Text ganando;
     public Text gol;
-    int golJug1;
-    int golJug2;
+    public GameObject panelDeGol;
+    int golJug1 = 0;
+    int golJug2 = 0; 
     string GolDeQuien;  
     // Start is called before the first frame update
     void Start()
     {
         golJug1 = System.Convert.ToInt32(puntaje1);
         golJug2 = System.Convert.ToInt32(puntaje2);
+
+        panelDeGol.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        while (golJug1 > golJug2)
-        {
-            ganando.text = "Jugador 1 está ganando";
-        }
-        while (golJug2 > golJug1)
-        {
-            ganando.text = "Jugador 2 está ganando";
-        }
-        while (golJug1 == golJug2)
-        {
-            ganando.text = "Están empatando";
-        }
-
+        // while (golJug1 > golJug2)
+        //  {
+        //    ganando.text = "Jugador 1 está ganando";
+        // }
+        // while (golJug2 > golJug1)
+        // {
+        //     ganando.text = "Jugador 2 está ganando";
+        //}
+        // while (golJug1 == golJug2)
+        // {
+        //     ganando.text = "Están empatando";
+        //  }
     }
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.name == "Arco1")
         {
-            golJug2++;
+            // Suma puntaje del marcador del jugador2
+            golJug2++; 
             puntaje2.text = golJug2.ToString();
+            // Se escucha el sonido de gol
             fuenteaudio.clip = SonidoGol;
             fuenteaudio.Play();
+            // Aparece el panel de gol diciendo que el jugador 1 hizo gol
+            panelDeGol.SetActive(true);
+            gol.text = "Gol de jugador 2";
+            //La pelota se resetea a la posición inicial
             transform.position = new Vector3(0, 0.85f, -0.55f);
-            gol.text = ("Gol de Jugador 2"); 
-            // gol jugador 2
+          
         }
+        
         if (col.gameObject.name == "Arco2")
         {
+            // Suma puntaje del marcador del jugador 1
             golJug1++;
             puntaje1.text = golJug1.ToString();
+            // Se escucha el sonido de gol
             fuenteaudio.clip = SonidoGol;
             fuenteaudio.Play();
+            // Aparece el panel de gol diciendo que el jugador 1 hizo gol
+            panelDeGol.SetActive(true);
+            gol.text = "Gol de jugador 1";
+            //La pelota se resetea a la posición inicial
             transform.position = new Vector3(0, 0.85f, -0.55f);
-            // gol jugador 1
+
         }
+        
     }
+    
+
 }
